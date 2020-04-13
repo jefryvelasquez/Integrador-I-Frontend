668861d6c3b28a6b31c3d62af05f86febabebc38
jefryvelasquez <jeferson.velasquez@udea.edu.co>
2020-04-12 22:12:52 -0500

    se eliminan modulos no utilizados

--- src/app/component/accordion/accordion.component.ts
+++ src/app/component/accordion/accordion.component.ts
@@ -5,14 +5,4 @@ import { NgbPanelChangeEvent } from '@ng-bootstrap/ng-bootstrap';
   selector: 'app-ngbd-accordion-basic',
   templateUrl: 'accordion.component.html'
 })
-export class NgbdAccordionBasicComponent {
-  beforeChange($event: NgbPanelChangeEvent) {
-    if ($event.panelId === 'preventchange-2') {
-      $event.preventDefault();
-    }
-
-    if ($event.panelId === 'preventchange-3' && $event.nextState === false) {
-      $event.preventDefault();
-    }
-  }
-}
+export class NgbdAccordionBasicComponent {}
